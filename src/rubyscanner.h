b029fbb7ddee56a492958e08d4425d8c6fb2afcb
Vincent Fourmond <vincent.fourmond@9online.fr>
2009-11-22 00:41:11 +0100

    Factor out the context-keeping code

--- src/rubyscanner.h
+++ src/rubyscanner.h
@@ -33,25 +33,64 @@
  */
 class RubyLanguageScanner : public ParserInterface
 {
+public:
+  virtual ~RubyLanguageScanner() {}
+  void parseInput(const char * fileName, 
+		  const char *fileBuf, 
+		  Entry *root);
+  bool needsPreprocessing(const QCString &extension);
+  void parseCode(CodeOutputInterface &codeOutIntf,
+		 const char *scopeName,
+		 const QCString &input,
+		 bool isExampleBlock,
+		 const char *exampleName=0,
+		 FileDef *fileDef=0,
+		 int startLine=-1,
+		 int endLine=-1,
+		 bool inlineFragment=FALSE,
+		 MemberDef *memberDef=0
+		 );
+  void resetCodeParserState();
+  void parsePrototype(const char *text);
+  
+  /// This is a small class for internal use whose job is to keep
+  /// information about the context in which the scanner is.
+  class ScannerContext {
   public:
-    virtual ~RubyLanguageScanner() {}
-    void parseInput(const char * fileName, 
-                    const char *fileBuf, 
-                    Entry *root);
-    bool needsPreprocessing(const QCString &extension);
-    void parseCode(CodeOutputInterface &codeOutIntf,
-                   const char *scopeName,
-                   const QCString &input,
-                   bool isExampleBlock,
-                   const char *exampleName=0,
-                   FileDef *fileDef=0,
-                   int startLine=-1,
-                   int endLine=-1,
-                   bool inlineFragment=FALSE,
-                   MemberDef *memberDef=0
-                  );
-    void resetCodeParserState();
-    void parsePrototype(const char *text);
+    /// Is is OK to end an expression on a newline ?
+    bool newLineOK;
+    
+    /// Is there a pending unescaped backspace ?
+    bool pendingBackspace;
+
+    /// Is it OK to begin a class/function definition ?
+    bool definitionOK;
+
+    /// Is the next thing surely an expression ?
+    bool surelyExpression;
+
+    /// True if the next thing cannot be a definition -- but could be
+    /// an expression.
+    bool noDefinition;
+
+    /// Can an expression be started ? 
+    bool expressionOK;
+
+    /// Whether if/until and so on start a new block ?
+    bool ifNewBlock; 
+
+    /// Whether or not we can finish the current expression
+    bool canEndExpression;
+
+    /// Reset the state to a Beginning-of-file one
+    void reset();
+
+    ScannerContext() { reset();};
+
+    
+    /// Parses one character and updates the state of the context.
+    void parseOneChar(char ch);
+  };
 };
 
 void rubyscanFreeScanner();
